5ed078738f2b34204eabfd145247bec0723062f9
Mariam Ehab <160272932+Marria-m@users.noreply.github.com>
2024-03-13 23:29:21 +0200

    Update rational calc.cpp
    Doha code

--- rational calc.cpp
+++ rational calc.cpp
@@ -1,9 +1,73 @@
-#include <iostream>
-using namespace std;
+#include<iostream>
+#include<iomanip>//setprecision
+#include <sstream>//ostrinstream
+using namespace std ;
+double fraction(double n)
+{
+    // Declare oss as std::ostringstream
+    ostringstream oss;
+    
+    // Write the formatted number to the stringstream
+    oss << fixed << setprecision(2) << n;
 
-int main(){
+    // Get the formatted string from the stringstream and convert it to double
+    double d = stod(oss.str());
 
+    // Multiply the formatted double by 100
+    double value = d * 100;
 
+    // Print the multiplied value
+    //cout <<"the result is " << value << "/" << "100" << endl;
+    int s = min(int(value), 100);
+    while(s > 0)
+    {
+        if(int(value) % s == 0 && 100%s == 0)
+        {
+         cout << "\n========\nresult\n========\n " <<endl << value/s << "\nــــ\n"<< 100/s <<endl;
+         return 0 ;
+        }
+        s-- ; 
+    }
+    cout <<"the result : " << value << "/" << "100" << endl;
 
     return 0;
+}
+int main()
+{
+    cout << "Enter the first number : " ;
+    double num1 , num2 , n;
+    cin >> num1 ;
+    cout << "Enter the second number : " ;
+    cin >> num2 ;
+    cout <<"Insert an operation (+,-,*,/): " ;
+    char oper ;
+    cin >> oper ;
+    while(oper != '+' && oper != '-' && oper != '*' && oper != '/' )
+    {
+        cout << "Please insert a valid operation : " ;
+        cin >> oper ;
+    }
+    switch(oper)
+    {
+        case '+' :
+            n = num1 + num2 ;
+            fraction(n) ;
+            break;
+        
+        case '-' :
+            n = num1 - num2 ;
+            fraction(n) ;
+            break;
+        
+        case '*':
+            n = num1 * num2 ;
+            fraction(n) ;
+            break;
+        
+        case '/':
+            n = num1 / num2 ;
+            fraction(n) ;
+            break;
+
+    }
 }
