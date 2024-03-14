ca206f14e472fca5f53eb7e1230f0b08fe42574a
Mariam Ehab <160272932+Marria-m@users.noreply.github.com>
2024-03-14 02:46:47 +0200

    Update rational calc.cpp
    مش أحسن حاجة يعني بس الي عرفت أوصله إلى الآن

--- rational calc.cpp
+++ rational calc.cpp
@@ -1,73 +1,41 @@
-#include<iostream>
-#include<iomanip>//setprecision
-#include <sstream>//ostrinstream
-using namespace std ;
-double fraction(double n)
-{
-    // Declare oss as std::ostringstream
-    ostringstream oss;
-    
-    // Write the formatted number to the stringstream
-    oss << fixed << setprecision(2) << n;
+#include <iostream>
+#include <string>
+using namespace std;
 
-    // Get the formatted string from the stringstream and convert it to double
-    double d = stod(oss.str());
-
-    // Multiply the formatted double by 100
-    double value = d * 100;
-
-    // Print the multiplied value
-    //cout <<"the result is " << value << "/" << "100" << endl;
-    int s = min(int(value), 100);
-    while(s > 0)
-    {
-        if(int(value) % s == 0 && 100%s == 0)
-        {
-         cout << "\n========\nresult\n========\n " <<endl << value/s << "\nــــ\n"<< 100/s <<endl;
-         return 0 ;
+void valid(string number, string string1,string string2){
+    int len = number.length();
+    for (int i = 0; i < len; i++){
+        if (number[i] == '/'){
+            if (len % 2 != 0) {
+                for (int j = len - i; j < len; j++) {
+                    string2 += number[j];
+                }
+                break;
+            }
+            else {
+                for (int j = len - i; j < len; j++) {
+                    string2 += number[j + 1];
+                }
+                break;
+            }
         }
-        s-- ; 
+        else {
+            string1 += number[i];
+        }
     }
-    cout <<"the result : " << value << "/" << "100" << endl;
-
-    return 0;
+    cout << string1 << "\n " << string2 << "\n";
 }
-int main()
-{
-    cout << "Enter the first number : " ;
-    double num1 , num2 , n;
-    cin >> num1 ;
-    cout << "Enter the second number : " ;
-    cin >> num2 ;
-    cout <<"Insert an operation (+,-,*,/): " ;
-    char oper ;
-    cin >> oper ;
-    while(oper != '+' && oper != '-' && oper != '*' && oper != '/' )
-    {
-        cout << "Please insert a valid operation : " ;
-        cin >> oper ;
-    }
-    switch(oper)
-    {
-        case '+' :
-            n = num1 + num2 ;
-            fraction(n) ;
-            break;
-        
-        case '-' :
-            n = num1 - num2 ;
-            fraction(n) ;
-            break;
-        
-        case '*':
-            n = num1 * num2 ;
-            fraction(n) ;
-            break;
-        
-        case '/':
-            n = num1 / num2 ;
-            fraction(n) ;
-            break;
 
-    }
+int main() {
+
+    
+   string num1, num2, str1_1, str2_1, str1_2, str2_2;
+    cout << "Enter the first number: ";
+    getline(cin, num1);
+    valid(num1, str1_1, str2_1);
+
+    cout << "Enter the second number: ";
+    getline(cin, num2);
+    valid(num2, str1_2, str2_2);
+     return 0;
 }
